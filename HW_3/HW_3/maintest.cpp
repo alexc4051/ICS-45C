d8d881f15937629aaf2a79a788b24d982a3af8ef
alexc4051 <pochienc@uci.edu>
2015-12-19 20:42:47 -0800

    Revert "45C HW"
    This reverts commit 2af84d32be8efc6ab037781841817d40b13e94f0.

--- HW_3/HW_3/maintest.cpp
+++
@@ -1,8 +0,0 @@
-#include "stringclass.h"
-
-int main()
-{
-	String s("alphabet");
-	String sec = s.reverse();
-	std::cout << s.size() << endl;
-}
--- tester/tester/Source.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-
-int main()
-{std::cout << "YOU SUCK";
-std::cout << "YOU IS SUCKY";
-}
